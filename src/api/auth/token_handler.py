12bff6e552a1a1a3958c95225763a711bfc498bc
veritas <veritate.dev@gmail.com>
2024-06-13 00:35:22 -0300

    refactor(token_handler-file): :recycle: Refacts token_handler into a better approach

--- src/api/auth/token_handler.py
+++ src/api/auth/token_handler.py
@@ -1,36 +1,27 @@
 from datetime import datetime, timedelta
 from decouple import config
-from jwt import encode, decode, ExpiredSignatureError, DecodeError
+from jwt import encode, ExpiredSignatureError, decode, DecodeError
 
-from .utils.token_generator import create_token
+from utils import Logger
 
+_logger = Logger(__name__)
 class TokenHandler:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is not None:
-            return cls._instance
-        cls._instance = super().__new__(cls)
-        return cls._instance
-
     def __init__(self):
-        self.secret = config('SECRET')
-        self.algorithm = config('ALGORITHM')
-    
-    def __repr__(self) -> str:
-        return f'Token handler instance: {id(self)}'
+        self.secret = config("SECRET")
+        self.algorithm = config("ALGORITHM")
 
-    def token_encoder(self) -> str:
-        payload = {
-            "exp" : datetime.now() + timedelta(seconds=3600),
-            "session_token" : create_token()
-        }
-        return encode(payload, self.secret, algorithm=self.algorithm)
-    
+    def __repr__(self) -> str:
+        return f"Token handler instance: {id(self)}"
+
+    def token_encoder(self, _user_id: int) -> str:
+        payload = {"exp": datetime.now() 
+                    + timedelta(seconds=3600), "sub": _user_id}
+        return encode(payload, self.secret, self.algorithm)
+
     def token_decoder(self, token: str) -> dict:
         try:
             return decode(token, self.secret, algorithms=[self.algorithm])
-        except ExpiredSignatureError:
-            return ValueError('Token expired')
-        except DecodeError:
-            return ValueError('Error on token decoding')
+        except ExpiredSignatureError as e:
+            _logger.error(f'Expired token try with token: {token} | Error: {str(e)}')
+        except DecodeError as e:
+            _logger.error(f'Error while trying to decode token: {token} | Error: {str(e)}')
